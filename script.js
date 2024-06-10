56be50e7f67b7343f6bd6048575f362aa94bdf30
Matheus Bortolotto <matheusbortolol@hotmail.com>
2024-06-10 17:54:34 -0300

    subindo arquivos do portifolio

---
+++ script.js
@@ -0,0 +1,29 @@
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const elements = document.querySelectorAll(".rolagem");
+
+elements.forEach((element) => myObserver.observe(element));
+
+let btnMenu = document.getElementById("btn-menu");
+let menu = document.getElementById("menu-mobile");
+let overlay = document.getElementById("overlay-menu");
+
+btnMenu.addEventListener("click", () => {
+  menu.classList.add("abrir-menu");
+});
+
+menu.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
